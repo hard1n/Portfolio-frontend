d2fd71908b12365b8128352ae348fe6a25beff3b
hard1n <hardisonpaulino@gmail.com>
2022-01-25 18:46:15 -0800

    Retos 1 al 4

---
+++ Reto2 - Article preview component/script.js
@@ -0,0 +1,23 @@
+const shareButton = document.querySelector('#share');
+
+shareButton.addEventListener('click', function() {
+        const modal = document.getElementById('modal-share');
+        if (modal.classList == "modal-share") {   
+            openModal(modal);
+            shareButton.style.backgroundColor = "hsl(217, 19%, 35%)";
+        } 
+        else {
+            closeModal(modal);
+            shareButton.style.backgroundColor = "hsl(210, 46%, 95%)";
+        }
+});
+
+function openModal(modal) {
+    if (modal == null) return
+    modal.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+    modal.classList.remove('active')
+}
