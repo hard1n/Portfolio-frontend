10bf14598e97fd314ba05f02d62a468a79b22598
hard1n <hardisonpaulino@gmail.com>
2022-01-28 18:59:50 -0800

    Testimonial HTML and CSS basic structure

---
+++ Reto4/main.js
@@ -0,0 +1,21 @@
+// var tanyaImg = document.createElement("img");
+// img.src = "./assets/imgs/image-john.jpg";
+// var src = document.querySelector(".user__picture");
+ 
+// src.appendChild(img);
+
+// console.log(src);
+
+var tanya = {
+    comment: "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+    userName: "Tanya Sinclair",
+    userProfession: "UX Engineer"
+};
+
+var john = {
+    comment: "I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future.",
+    userName: "Tanya Sinclair",
+    userProfession: "UX Engineer"
+};
+
+console.log(tanya);
