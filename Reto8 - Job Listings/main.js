2c0f3fd34d4f1eff6a8c2d507417967ced56f1b6
hard1n <hardisonpaulino@gmail.com>
2022-02-27 19:17:21 -0800

    HMTL structure

---
+++ Reto8 - Job Listings/main.js
@@ -0,0 +1,23 @@
+const c = console.log;
+
+const jobs = [
+    {
+        id: 1,
+        logoImg: "./assets",
+        company: "Photosnap",
+        jobTile: "Senior Frontend Developer",
+        uploadDate: "1d ago",
+        workTime: "Full Time",
+        WorkLocation: "USA ",
+    },
+];
+
+const skillSets = [
+    {
+        sk1: "HTML",
+        sk2: "CSS",
+        sk3: "JavaScript"
+    },
+];
+
+c(jobs, skillSets);
